f7b53801e4b3fe9be2166ddda2debc50c34855ea
alsoshubham <shubhamsahu100100@hotmail.com>
2024-11-10 21:00:13 +0530

    .

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,1 +1,67 @@
-import conf from "../conf/conf";
+/* eslint-disable no-undef */
+/* eslint-disable no-useless-catch */
+import conf from '../conf/conf.js';
+
+import {Client, Account, ID} from "appwrite";
+
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.account =  new Account(this.client);
+    }
+
+    async createAccount({email, password, name}){
+        try {
+            await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.login({email, password});
+            } else {
+                return console.log(error);
+                
+            }
+        } 
+        catch (error) {
+         throw error;   
+        }
+    }
+
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            return console.log(error);
+        }
+        
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service  :: getCurrentUser :: error", error)
+        }
+        return null;
+    }
+
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Apwrite service :: logout :: error", error)
+        }
+    }
+}
+
+const authService =  new AuthService();
+
+
+export default authService;
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -3,6 +3,10 @@ const conf = {
     appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
-    appwroteBuckteId: String(import.meta.env.VITE_BUCKET_ID),
+    appwriteBuckteId: String(import.meta.env.VITE_BUCKET_ID),
 }
 export default conf
+
+
+// conf humne isliya kiya taaki .env vaaali file ki values string format me aa jaaye aur humein
+// baar baar dependencies me na jana pde aur optimised format me ek value as an output aaye
