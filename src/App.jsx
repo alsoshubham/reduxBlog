9579666c980c17c1c3d2e9d6d2a9523852ba0872
alsoshubham <shubhamsahu100100@hotmail.com>
2024-11-11 15:53:44 +0530

    created the features, components, and containers

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import "./App.css";
 import authService from ".//appwrite/auth"
 import {login, logout} from "./store/authSlice"
+import {Header, Footer} from "./components/index"
+import { Outlet } from "react-router-dom";
 function App() {
 
   const [loading, setLoading] = useState(true)
@@ -17,12 +19,20 @@ function App() {
       }
     })
     .finally(() => setLoading(false))
-  }, [])
-  return (
-    <>
-      <h1>hello appwrite</h1>
-    </>
-  );
+  })
+
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
---
+++ src/components/container/Container.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+
+function Container({props}) {
+    return <div className='w-full max-w-7xl mx-auto px-4'>{props}</div>;
+  }
+  
+  export default Container;
+  
