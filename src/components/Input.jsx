268029499f31fe9c9981457b203ae7e76759e223
alsoshubham <shubhamsahu100100@hotmail.com>
2024-11-18 14:40:26 +0530

    .

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, {useId} from 'react'
-
+// doubt in input.jsx
 const Input = React.forwardRef( function Input({
     label,
     type = "text",
@@ -18,7 +18,9 @@ const Input = React.forwardRef( function Input({
             }
             <input
             type={type}
-            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none
+                focus:bg-gray-50 duration-200 border border-gray-200 w-full 
+                ${className}`}
             ref={ref}
             {...props}
             id={id}
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,26 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    status : false,
-    userData: null
-}
-
+  status: false,
+  userData: null,
+};
 
 const authSlice = {
-    name : "auth",
-    initialState,
-    reducers: {
-        // inside reducers, login and logout are two types of actions to be performed
-        login: (state, action) => {
-            state.status = true,
-            state.userData = action.payload.userData;
-        },
-        logout: (state) => {
-            state.status = false,
-            state.userData = null;
-        }
-    }
-}
+  name: "auth",
+  initialState,
+  reducers: {
+    // inside reducers, login and logout are two types of actions to be performed
+    login: (state, action) => {
+      (state.status = true), (state.userData = action.payload.userData);
+    },
+    logout: (state) => {
+      (state.status = false), (state.userData = null);
+    },
+  },
+};
 
-export const {login, logout} = authSlice.reducers
+export const { login, logout } = authSlice.reducers;
 export default authSlice.reducers;
